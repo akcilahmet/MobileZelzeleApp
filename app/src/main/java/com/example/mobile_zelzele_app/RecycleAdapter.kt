056a9235870281224664df3bc38701099449d808
Ahmet Akçıl <83040124+akcilahmet@users.noreply.github.com>
2023-03-16 18:09:11 +0300

    Adapter created for recycleView connection. And the adjustments were made

---
+++ app/src/main/java/com/example/mobile_zelzele_app/RecycleAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.mobile_zelzele_app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecycleAdapter(val newsList:ArrayList<News>) :RecyclerView.Adapter<RecycleAdapter.ZelzeleVH>(){
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ZelzeleVH {
+        val itemView= LayoutInflater.from(parent.context).inflate(R.layout.recycler_row,parent,false)
+        return ZelzeleVH(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ZelzeleVH, position: Int) {
+        holder.recyclerViewCityName.text=newsList[position].cityName
+        holder.recyclerViewCityDistrictName.text=newsList[position].districtName
+        holder.recyclerViewMagnitude.text=newsList[position].magnitude
+        holder.recyclerViewDepth.text=newsList[position].depth
+        holder.recyclerViewTime.text=newsList[position].time
+        holder.recyclerViewPassingTime.text=newsList[position].passingTime
+    }
+
+    override fun getItemCount(): Int {
+        return newsList.size
+    }
+
+    class ZelzeleVH(itemView:View):RecyclerView.ViewHolder(itemView) {
+        val recyclerViewCityName: TextView = itemView.findViewById(R.id.cityName)
+        val recyclerViewCityDistrictName: TextView = itemView.findViewById(R.id.cityDistrictName)
+        val recyclerViewMagnitude: TextView = itemView.findViewById(R.id.magnitude)
+        val recyclerViewDepth: TextView = itemView.findViewById(R.id.depth)
+        val recyclerViewTime: TextView = itemView.findViewById(R.id.time)
+        val recyclerViewPassingTime: TextView = itemView.findViewById(R.id.passingTime)
+    }
+}
