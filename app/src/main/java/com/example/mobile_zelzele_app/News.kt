a2f18cb4205f4c72b7bd2a052361b648d2d579c5
Ahmet Akçıl <83040124+akcilahmet@users.noreply.github.com>
2023-03-16 18:07:30 +0300

    News data class has been created for incoming data data
    Define the data needed for the incoming data

---
+++ app/src/main/java/com/example/mobile_zelzele_app/News.kt
@@ -0,0 +1,3 @@
+package com.example.mobile_zelzele_app
+
+data class News(var cityName: String, var districtName :String, var magnitude:String,var depth:String,var time:String,var passingTime:String)
